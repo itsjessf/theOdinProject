32db7f651142af3ee3355799ddd4c4a9fc126e53
itsjessf <jessica.pferreira@outlook.com>
2022-03-16 15:15:49 +0100

    Jest is pretty cool. Tests finished

--- odin-tests-jets/odin.js
+++ odin-tests-jets/odin.js
@@ -1,4 +1,64 @@
-function sum(a, b) {
-  return a + b;
+function capitalize(word) {
+  if (word.includes(" ")) {
+    return;
+  }
+  const lowerCaseWord = word.toLowerCase();
+  return lowerCaseWord.charAt(0).toUpperCase() + lowerCaseWord.slice(1);
 }
-module.exports = sum;
+
+function reverseString(word) {
+  let reversedWord = [];
+  for (let i = 0; i < word.length; i++) {
+    reversedWord.push(word[word.length - (i + 1)]);
+  }
+  return reversedWord.join("");
+}
+
+const calculator = {
+  add: (a, b) => {
+    return a + b;
+  },
+
+  subtract: (a, b) => {
+    return a - b;
+  },
+
+  multiply: (a, b) => {
+    return a * b;
+  },
+
+  divide: (a, b) => {
+    return a / b;
+  },
+};
+
+function analyseArray(values) {
+  let analisedArray = {};
+  const arraySum = values.reduce((accumulator, currentNumber) => accumulator + currentNumber);
+  const average = arraySum/values.length;
+  let smallestNumber= Number.MAX_SAFE_INTEGER;
+  let biggestNumber = Number.MIN_SAFE_INTEGER;
+
+  for (let i = 0; i < values.length; i++) {
+    if(values[i] < smallestNumber){
+      smallestNumber = values[i];
+    }
+    if(values[i] > biggestNumber){
+      biggestNumber = values[i]
+    }
+  }
+
+  return {
+    average: average,
+    min: smallestNumber,
+    max: biggestNumber,
+    length: values.length
+  }
+}
+
+module.exports = {
+  capitalize,
+  reverseString,
+  calculator,
+  analyseArray,
+};
--- odin-tests-jets/odin.test.js
+++ odin-tests-jets/odin.test.js
@@ -1,5 +1,29 @@
-const sum = require("./odin");
+const {capitalize, reverseString, calculator, analyseArray} = require("./odin");
 
-test("adds 1 + 2 to equal 3", () => {
-  expect(sum(1, 2)).toBe(3);
+test('computer_to_Computer', () => {
+  expect(capitalize('computer')).toBe('Computer');
+});
+
+test('strings_not_allowed', () => {
+  expect(capitalize('this is a sentence')).toBe(undefined);
+})
+
+test('abcd_to_dcba', ()=> {
+  expect(reverseString('abcd')).toBe('dcba');
+})
+
+test('should_do_calculations', () => {
+  expect(calculator.add(1,1)).toBe(2);
+  expect(calculator.subtract(1,1)).toBe(0);
+  expect(calculator.divide(2,2)).toBe(1);
+  expect(calculator.multiply(5, 2)).toBe(10);
+});
+
+test('should_analyze_array ', () => {
+  expect(analyseArray([1,8,3,4,2,6])).toMatchObject({
+    average: 4,
+    min: 1,
+    max: 8,
+    length: 6
+  });
 });
